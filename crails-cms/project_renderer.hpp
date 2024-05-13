ec6c585622eb8421f37e5b5961a9eacc1c947ea3
plaristote <michael@unetresgrossebite.com>
2024-05-13 14:49:41 +0200

    bump libcrails-renderer

--- crails-cms/project_renderer.hpp
+++ crails-cms/project_renderer.hpp
@@ -7,6 +7,7 @@ class ProjectRenderer : public Crails::Renderer
 {
 public:
   ProjectRenderer();
+  std::string_view get_name() const override { return "crails-cms/ProjectRenderer"; }
   const std::vector<std::string>& get_mimetypes() const override;
   bool can_render(const std::string& view) const override { return templates.find(view) != templates.end(); }
   void render_template(const std::string& view, Crails::RenderTarget& target, Crails::SharedVars& vars) const override
